1c2065e29de878d32c67face4c20c6564ced0165
David Inga <david.inga@vizzuality.com>
2019-02-14 10:37:08 +0100

    Updates

---
+++ precache-manifest.1117b428f99ecf1c15fb9e975739706b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7baccafc100a8b03dbc",
+    "url": "/static/js/main.d7baccaf.chunk.js"
+  },
+  {
+    "revision": "42e85152a4940ea3ab49",
+    "url": "/static/js/2.42e85152.chunk.js"
+  },
+  {
+    "revision": "d7baccafc100a8b03dbc",
+    "url": "/static/css/main.be2a11a2.chunk.css"
+  },
+  {
+    "revision": "985a363972f723b48fa63e058e229194",
+    "url": "/index.html"
+  }
+];
