ed1baa123ecc208ae4f1d4882367f76501a62817
David Inga <david.inga@vizzuality.com>
2019-02-14 12:05:16 +0100

    Updates

--- precache-manifest.dfb7a044f8c3fb35a4aa6ce1b49eb8d9.js
+++ precache-manifest.dfb7a044f8c3fb35a4aa6ce1b49eb8d9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ruffle-generator/static/js/runtime~main.95408cc0.js"
   },
   {
-    "revision": "f613c361d8aa2adaf4ac",
-    "url": "/ruffle-generator/static/js/main.f613c361.chunk.js"
+    "revision": "9721154b1991dd009d0c",
+    "url": "/ruffle-generator/static/js/main.9721154b.chunk.js"
   },
   {
     "revision": "d542bc8ffce10f456271",
     "url": "/ruffle-generator/static/js/2.d542bc8f.chunk.js"
   },
   {
-    "revision": "f613c361d8aa2adaf4ac",
-    "url": "/ruffle-generator/static/css/main.6677cbd8.chunk.css"
+    "revision": "9721154b1991dd009d0c",
+    "url": "/ruffle-generator/static/css/main.9272049a.chunk.css"
   },
   {
-    "revision": "8a2123f47f9cf752ebb9a2f98422c887",
+    "revision": "e539fe1913aaaefbed704b828d656d10",
     "url": "/ruffle-generator/index.html"
   }
 ];
